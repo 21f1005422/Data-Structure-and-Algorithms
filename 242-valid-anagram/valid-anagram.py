a199c6b8f1c453380f34dded7bd022bc91db8f43
yash12agarwal <21f1005422@ds.study.iitm.ac.in>
2026-01-07 00:02:49 +0530

    Time: 11 ms (74.25%) | Memory: 19.5 MB (8.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,11 +3,13 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        for ch in s:
-            if s.count(ch) != t.count(ch):
-                return False
-        
-        return True
+        dict_s={}
+        dict_t={}
+        for i in s:
+            dict_s[i] = dict_s.get(i, 0) + 1
+        for i in t:
+            dict_t[i] = dict_t.get(i, 0) + 1
+        return dict_s==dict_t
 
 
         
